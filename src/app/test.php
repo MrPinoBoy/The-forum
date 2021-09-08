39d4f1d80605f3e4c5fe4d4fab47575f4e2a218a
Will Dvl <william.deville13@gmail.com>
2021-09-08 16:20:08 +0200

    encore +++ CLEAN

--- src/app/test.php
+++
@@ -1,8 +0,0 @@
-<form method="post" action="./Controller/UserController.php">
-    <input type="text" name="nickname"></input>
-    <input type="text" name="email"></input>
-    <input type="text" name="password"></input>
-    <input type="file" name="avatar"></input>
-    <input type="text" name="signature"></input>
-    <input type="submit"></input>
-</form>
