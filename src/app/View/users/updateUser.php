007e650f7ea25b045a26fb220d6651709c594a9a
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-15 09:55:41 +0200

    added informations into update page

--- src/app/View/users/updateUser.php
+++ src/app/View/users/updateUser.php
@@ -1,12 +1,13 @@
 <?php require '../app/View/includes/header.php'?>
 
 <form method="POST" action="./index.php?page=updateprofile" enctype="multipart/form-data"><!-- php sends the form values to this page-->
-    <input type="text" name="updateNickname" placeholder="Username"></input>
+    <input type="text" name="updateNickname" placeholder="Username" value="<?=$_SESSION['loggedUser']?>"></input>
     <input type="text" name="updatePassword" placeholder="Password"></input>
+    <img src="<?= $_SESSION['loggedAvatar']?>">
     <input type="file" name="updateAvatar" placeholder="notrequired"></input>
     <label for="defaultAvatar">Check if you want a default avatar</label>
     <input type="checkbox" name="defaultAvatar" value="true"></input>
-    <input type="text" name="updateSignature" placeholder="notrequired"></input>
+    <input type="text" name="updateSignature" placeholder="notrequired" value="<?=$_SESSION['loggedSignature']?>"></input>
     <input type="submit"></input>
 </form>
 
