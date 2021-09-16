5e0c3ceea8e45e05f39b1eb9b9cfe31e3369e74a
Corentine4 <renuart.corentine@gmail.com>
2021-09-16 16:06:45 +0200

    Push Wetransfer

--- src/app/Controller/UserController.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //models
-    require_once "../Model/NewUser.php";//on va chercher les fichiers nécessaires au script
-    //controllers
-
-    //config
-    require_once "../config/config.php";
-
-    (new NewUser($_POST["nickname"]))->createUser();//on crée un nouvel utilisateur et on lui applique la fonction createUser avec les paramêtres insérés
