0dabf1afab9a8e8d086d3fbd2a1caa88089c12c1
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-17 15:56:02 +0200

    pathing

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
         <title>test</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="./public/css/style.css">
         <script src="https://kit.fontawesome.com/953d8595bf.js" crossorigin="anonymous"></script>
     </head>
     <body>
-      <?php  require_once '../app/require.php'; ?>
+      <?php  require_once './app/require.php'; ?>
     </body>
 </html>
