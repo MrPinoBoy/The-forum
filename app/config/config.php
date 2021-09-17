af55bc1ef33d6dc627d65b1296bb79b91ad26eed
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-17 16:08:04 +0200

    db settings

--- app/config/config.php
+++ app/config/config.php
@@ -3,12 +3,13 @@
     //Paramêtres de la base de données (ils se trouvent dans le fichier docker-compose.yml)
     //dès que ce fichier est require, les variables DB_HOST, DB_USER etc seront utilisable comme des constantes
     define('DB_HOST','mysql');
-    define('DB_USER','root');
-    define('DB_PASS','root');
-    define('DB_NAME','forum');
+    define('DB_USER','forum-nainportequoi');
+    define('DB_PASS','NvQhFktHXTtdV3uB');
+    define('DB_NAME','forum-nainportequoi');
 
     //URLROOT (Dynamic links)
-    define('URLROOT','localhost');
+    define('URLROOT','http://188.166.24.55/');
+
 
     //Sitename
     define('SITENAME','Forum - BeCode');
