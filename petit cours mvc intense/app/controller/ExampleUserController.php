6fc27c5b9c778ddb5d8627141b5fc14292a3de0e
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-17 15:54:07 +0200

    cleanup

--- petit cours mvc intense/app/controller/ExampleUserController.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class UserController
-    {
-        public function create(){
-            (new User($_POST['nickname'],$_POST['email'],$_POST['password'],$_POST['avatar'],$_POST['signature']))->createUser();
-        }
-        
-        require_once "../app/view/users/ExampleCreateUser";
-    }
