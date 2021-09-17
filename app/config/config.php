2c99a2db0578aefd3b2f34b1695d0deafe525034
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-17 16:42:29 +0200

    urlroot

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 
     //Paramêtres de la base de données (ils se trouvent dans le fichier docker-compose.yml)
     //dès que ce fichier est require, les variables DB_HOST, DB_USER etc seront utilisable comme des constantes
-    define('DB_HOST','mysql');
+    define('DB_HOST','188.166.24.55');
     define('DB_USER','forum-nainportequoi');
     define('DB_PASS','NvQhFktHXTtdV3uB');
     define('DB_NAME','forum-nainportequoi');
